229064546f160954fe99022051101065bc12d72e
Frank10OC <111895356+Frank10OC@users.noreply.github.com>
2023-06-22 22:23:44 -0500

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -29,7 +29,7 @@ def main():
     st.title("Mostrar datos de Google Drive en Streamlit")
 
     # Enlace compartido del archivo en Google Drive
-    file_url = "https://drive.google.com/drive/my-drive"
+    file_url = "https://drive.google.com/file/d/1ea3baHPePunw8bWNeAzygsfV9YXYI6Mc/view?usp=drive_link"
 
     # Descargar el archivo desde Google Drive
     output_file = "data.csv"
