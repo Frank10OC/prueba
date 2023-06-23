7f778bec1be38d01f678c545c44b43d6d5d7abb8
Frank10OC <111895356+Frank10OC@users.noreply.github.com>
2023-06-22 22:28:36 -0500

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -1,47 +1,27 @@
 import streamlit as st
-import pandas as pd
-import gdown
+import requests
 
 def main():
-    st.title("Cargar CSV desde Google Drive en Streamlit")
+    st.title("Obtener datos de una página web en Streamlit")
 
-    # Enlace compartido del archivo CSV en Google Drive
-    csv_url = "https://drive.google.com/file/d/1ea3baHPePunw8bWNeAzygsfV9YXYI6Mc/view?usp=drive_link"
+    # URL de la página web
+    url = "https://www.datosabiertos.gob.pe/dataset/casos-positivos-por-covid-19-ministerio-de-salud-minsa/resource/690e57a6-a465-47d8-86fd"
 
-    # Descargar el archivo CSV desde Google Drive
-    output_csv = "data.csv"
-    gdown.download(csv_url, output_csv, quiet=False)
+    # Realizar la solicitud HTTP a la página
+    response = requests.get(url)
 
-    # Cargar el archivo CSV en un DataFrame
-    df = pd.read_csv(output_csv)
+    if response.status_code == 200:
+        # Procesar la respuesta
+        data = response.text
 
-    # Mostrar el DataFrame
-    st.subheader("Contenido del archivo CSV")
-    st.write(df)
+        # Mostrar los datos en Streamlit
+        st.subheader("Contenido de la página web")
+        st.text(data)
+    else:
+        st.error(f"Error al obtener los datos. Código de estado: {response.status_code}")
 
 if __name__ == "__main__":
     main()
-import streamlit as st
-import pandas as pd
-import gdown
 
-def main():
-    st.title("Mostrar datos de Google Drive en Streamlit")
 
-    # Enlace compartido del archivo en Google Drive
-    file_url = "https://drive.google.com/file/d/1ea3baHPePunw8bWNeAzygsfV9YXYI6Mc/view?usp=drive_link"
-
-    # Descargar el archivo desde Google Drive
-    output_file = "data.csv"
-    gdown.download(file_url, output_file, quiet=False)
-
-    # Cargar el archivo en un DataFrame
-    df = pd.read_csv(output_file)
-
-    # Mostrar el DataFrame
-    st.subheader("Contenido del archivo")
-    st.write(df)
-
-if __name__ == "__main__":
-    main()
 
