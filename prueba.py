b83178b8eb76495a3091258cdd2a12639b662a0e
Frank10OC <111895356+Frank10OC@users.noreply.github.com>
2023-06-22 20:55:34 -0500

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
 
-dfa = pd.read_csv("https://datosabiertos.senace.gob.pe/home/VistaDatos/carteraproyectos?dataset=ESTUDIOS%20AMBIENTALES%20(APROBADO)&id=1&q=APROBADO#data")
+dfa = pd.read_csv("https://raw.githubusercontent.com/Frank10OC/prueba/main/Reporte_Proyecto_APROBADO%20(4).csv")
 # Mostrar el DataFrame en la aplicación de Streamlit
 st.write(dfa)
