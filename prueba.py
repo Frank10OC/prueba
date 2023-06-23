314d7c662c5d414c590e17d661a92bcbbab8ff2c
Frank10OC <111895356+Frank10OC@users.noreply.github.com>
2023-06-22 21:59:34 -0500

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -1,38 +1,23 @@
 import streamlit as st
 import pandas as pd
-
-dfa = pd.read_csv("https://raw.githubusercontent.com/Frank10OC/prueba/main/Reporte_Proyecto_APROBADO%20(4).csv")
-# Mostrar el DataFrame en la aplicación de Streamlit
-st.write(dfa)
-import streamlit as st
-import pandas as pd
-import folium
+import gdown
 
 def main():
-    st.title("Visualizador de Coordenadas en Mapa")
+    st.title("Cargar CSV desde Google Drive en Streamlit")
 
-    # Cargar el DataFrame
-    df = dfa # Función para cargar el DataFrame, puedes adaptarla según tus necesidades
+    # Enlace compartido del archivo CSV en Google Drive
+    csv_url = "https://drive.google.com/uc?id=your_file_id"
+
+    # Descargar el archivo CSV desde Google Drive
+    output_csv = "data.csv"
+    gdown.download(csv_url, output_csv, quiet=False)
+
+    # Cargar el archivo CSV en un DataFrame
+    df = pd.read_csv(output_csv)
 
     # Mostrar el DataFrame
-    st.subheader("DataFrame")
-    st.dataframe(df)
-
-    # Obtener las columnas de coordenadas
-    lat_column = df["LATITUD"]
-    lon_column = df["LONGITUD"]
-
-    # Crear el mapa
-    map = folium.Map()
-
-    # Agregar marcadores al mapa
-    for index, row in df.iterrows():
-        lat = row[lat_column]
-        lon = row[lon_column]
-        folium.Marker(location=[lat, lon], popup=f"Ubicación {index}").add_to(map)
-
-    # Mostrar el mapa
-    folium_static(map)
+    st.subheader("Contenido del archivo CSV")
+    st.write(df)
 
 if __name__ == "__main__":
     main()
