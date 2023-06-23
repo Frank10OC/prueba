3f64c044d471e410df8ef4b7beacc8416893c9a3
Frank10OC <111895356+Frank10OC@users.noreply.github.com>
2023-06-22 20:54:02 -0500

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -1,26 +1,6 @@
 import streamlit as st
 import pandas as pd
 
-def main():
-    st.title("Convertir JSON a CSV")
-
-    # Cargar el archivo JSON
-    file = st.file_uploader("Cargar archivo JSON", type=["json"])
-    if file is not None:
-        content = file.read()
-        try:
-            # Convertir JSON a DataFrame
-            df = pd.read_json(content)
-
-            # Convertir DataFrame a CSV
-            csv_data = df.to_csv(index=False)
-
-            # Descargar el archivo CSV resultante
-            st.download_button("Descargar CSV", data=csv_data, file_name='archivo.csv')
-
-        except pd.errors.JSONDecodeError:
-            st.error("Error al decodificar el archivo JSON.")
-
-if __name__ == "__main__":
-    main()
-
+dfa = pd.read_csv("https://datosabiertos.senace.gob.pe/home/VistaDatos/carteraproyectos?dataset=ESTUDIOS%20AMBIENTALES%20(APROBADO)&id=1&q=APROBADO#data")
+# Mostrar el DataFrame en la aplicación de Streamlit
+st.write(dfa)
