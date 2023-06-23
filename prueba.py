afffa65e9d4dbe89a1898bce372703417987a01e
Frank10OC <111895356+Frank10OC@users.noreply.github.com>
2023-06-22 21:27:56 -0500

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -4,10 +4,23 @@ import pandas as pd
 dfa = pd.read_csv("https://raw.githubusercontent.com/Frank10OC/prueba/main/Reporte_Proyecto_APROBADO%20(4).csv")
 # Mostrar el DataFrame en la aplicación de Streamlit
 st.write(dfa)
+import streamlit as st
+import pandas as pd
+import folium
+
 def main():
+    st.title("Visualizador de Coordenadas en Mapa")
+
+    # Cargar el DataFrame
+    df = cargar_dataframe()  # Función para cargar el DataFrame, puedes adaptarla según tus necesidades
+
+    # Mostrar el DataFrame
+    st.subheader("DataFrame")
+    st.dataframe(df)
+
     # Obtener las columnas de coordenadas
-    lat_column = st.selectbox("LATITUD", dfa.columns)
-    lon_column = st.selectbox("LONGITUD", dfa.columns)
+    lat_column = st.selectbox("Columna de latitud", df.columns)
+    lon_column = st.selectbox("Columna de longitud", df.columns)
 
     # Crear el mapa
     map = folium.Map()
@@ -21,5 +34,5 @@ def main():
     # Mostrar el mapa
     folium_static(map)
 
-a=main()
-
+if __name__ == "__main__":
+    main()
