5b0af92e04b94d8adf3bbe3b1e8d69c83dbcc2b3
Frank10OC <111895356+Frank10OC@users.noreply.github.com>
2023-06-22 22:32:08 -0500

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -1,27 +1,61 @@
 import streamlit as st
 import requests
+import pandas as pd
+from io import StringIO
 
 def main():
-    st.title("Obtener datos de una página web en Streamlit")
+    st.title("Buscar y cargar archivo CSV desde una página web en Streamlit")
 
-    # URL de la página web
-    url = "https://www.datosabiertos.gob.pe/dataset/casos-positivos-por-covid-19-ministerio-de-salud-minsa/resource/690e57a6-a465-47d8-86fd"
+    # URL de la página web donde se encuentra el archivo CSV
+    url = "https://www.example.com"
 
-    # Realizar la solicitud HTTP a la página
-    response = requests.get(url)
+    try:
+        # Realizar la solicitud HTTP a la página
+        response = requests.get(url)
 
-    if response.status_code == 200:
-        # Procesar la respuesta
-        data = response.text
+        if response.status_code == 200:
+            # Obtener el contenido de la respuesta como texto
+            html_content = response.text
 
-        # Mostrar los datos en Streamlit
-        st.subheader("Contenido de la página web")
-        st.text(data)
-    else:
-        st.error(f"Error al obtener los datos. Código de estado: {response.status_code}")
+            # Buscar el enlace del archivo CSV en el contenido de la página
+            csv_link = buscar_enlace_csv(html_content)
+
+            if csv_link:
+                # Realizar la solicitud HTTP al enlace del archivo CSV
+                csv_response = requests.get(csv_link)
+
+                if csv_response.status_code == 200:
+                    # Leer el archivo CSV en un DataFrame
+                    csv_data = csv_response.text
+                    df = pd.read_csv(StringIO(csv_data))
+
+                    # Mostrar los datos en Streamlit
+                    st.subheader("Contenido del archivo CSV")
+                    st.write(df)
+                else:
+                    st.error(f"Error al obtener el archivo CSV. Código de estado: {csv_response.status_code}")
+            else:
+                st.error("No se encontró el enlace del archivo CSV en la página")
+        else:
+            st.error(f"Error al obtener la página web. Código de estado: {response.status_code}")
+    except Exception as e:
+        st.error(f"Error en la solicitud HTTP: {e}")
+
+def buscar_enlace_csv(html_content):
+    # Aquí puedes utilizar técnicas de web scraping o parsing específicas para buscar el enlace del archivo CSV en el contenido HTML de la página
+    # Por ejemplo, puedes utilizar BeautifulSoup o expresiones regulares
+    # En este ejemplo, asumiremos que el enlace del archivo CSV está en una etiqueta <a> con el texto "Descargar CSV"
+    # Debes adaptar esta lógica a la estructura de la página web que estás consultando
+
+    # Ejemplo de búsqueda utilizando BeautifulSoup:
+    # from bs4 import BeautifulSoup
+    # soup = BeautifulSoup(html_content, "html.parser")
+    # csv_link = soup.find("a", text="Descargar CSV")["href"]
+
+    # En este ejemplo, simplemente devolvemos un enlace de ejemplo
+    csv_link = "https://www.example.com/data.csv"
+    return csv_link
 
 if __name__ == "__main__":
     main()
-
-
 
