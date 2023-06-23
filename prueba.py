ec7d3a0091c73fea61099dba5ca05c167cc1ed84
Frank10OC <111895356+Frank10OC@users.noreply.github.com>
2023-06-22 22:13:59 -0500

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -6,7 +6,7 @@ def main():
     st.title("Cargar CSV desde Google Drive en Streamlit")
 
     # Enlace compartido del archivo CSV en Google Drive
-    csv_url = "https://drive.google.com/uc?id=your_file_id"
+    csv_url = "https://drive.google.com/file/d/1ea3baHPePunw8bWNeAzygsfV9YXYI6Mc/view?usp=drive_link"
 
     # Descargar el archivo CSV desde Google Drive
     output_csv = "data.csv"
